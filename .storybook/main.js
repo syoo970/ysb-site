d320869c7be83cf764a3033fc6e5dceee67fb45f
syoo970 <syu9710@gmail.com>
2023-01-08 19:15:18 +0900

    chore: 기본적인 세팅

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-next",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "../");
+    return { ...config };
+  },
+};
---
+++ components/Test/index.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+    return <div>Hello! This is Test Component</div>
+};
+
+export default Test;
